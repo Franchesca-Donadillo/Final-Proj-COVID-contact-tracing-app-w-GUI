63fa100104c7d2a860f1566c4649b9a920f599c5
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-07-23 22:17:41 +0800

    name

--- contact tracing app - main.py
+++ contact tracing app - main.py
@@ -16,6 +16,9 @@ def win1():
         start_btn.destroy() 
         btn1 = tk.Button (title, text = "Submit", width=8, height=2, activebackground= "cyan" ) 
         btn1.place (x=300, y=620)
+        lbl_name = tk.Label (title, text = "name")
+        lbl_name.place(x=10, y=15)
+
     start_btn = tk.Button (title, text = "Start", width=15, height=4, activebackground= "cyan", command=win2 ) 
     start_btn.place (x=280, y=480)
 
