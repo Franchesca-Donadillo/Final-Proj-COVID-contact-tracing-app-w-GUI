b19103d0d4a78dccfa0f5de647df15d8b18ac8dd
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-07-24 00:41:06 +0800

    info vax, close contact, symptoms, window was resized

--- contact tracing app - main.py
+++ contact tracing app - main.py
@@ -9,13 +9,13 @@ title = tk.Tk()
 
 # create main window
 title.title("COVID CONTACT TRACING")
-title.geometry("670x670")
+title.geometry("500x500")
 
 def win1():
     def win2():
         start_btn.destroy() 
         btn1 = tk.Button (title, text = "Submit", width=8, height=2, activebackground= "cyan" ) 
-        btn1.place (x=300, y=620)
+        btn1.place (x=210, y=450)
         
         # name info
         lbl_name = tk.Label (title, text = "Name")
@@ -47,9 +47,32 @@ def win1():
         entry_add = tk.Entry (title, width="30")
         entry_add.place(x=70,y=175)
 
+        # vaccination
+        lbl_vax = tk.Label (title, text = "Have you been vaccinated?")
+        lbl_vax.place(x=10, y=205)
+        chk_vax1 = tk.Checkbutton(title, text="Yes", activebackground="green")
+        chk_vax1.place(x=170,y=205)
+        chk_vax2 = tk.Checkbutton(title, text="No", activebackground="green")
+        chk_vax2.place(x=215,y=205)
+
+        # close contact
+        lbl_con = tk.Label (title, text = "Have you had exposure to a probable or confirmed case in the last 14 days?")
+        lbl_con.place(x=10, y=240)
+        chk_con = tk.Checkbutton(title, text="Yes", activebackground="green")
+        chk_con.place(x=170,y=260)
+        chk_con = tk.Checkbutton(title, text="No", activebackground="green")
+        chk_con.place(x=215,y=260)
+
+        # experience symptoms
+        lbl_sym = tk.Label (title, text = "Are you experiencing any COVID-19 symptoms in the past 7 days?")
+        lbl_sym.place(x=10, y=290)
+        chk_sym = tk.Checkbutton(title, text="Yes", activebackground="green")
+        chk_sym.place(x=170,y=310)
+        chk_sym = tk.Checkbutton(title, text="No", activebackground="green")
+        chk_sym.place(x=215,y=310)
 
     start_btn = tk.Button (title, text = "Start", width=15, height=4, activebackground= "cyan", command=win2 ) 
-    start_btn.place (x=280, y=480)
+    start_btn.place (x=188, y=310)
 
 win1()
 title.mainloop()
