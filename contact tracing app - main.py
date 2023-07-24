527cf4914984e5f642ed8a4ca67fabf5bf458f19
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-07-24 16:54:25 +0800

    minor changes

--- contact tracing app - main.py
+++ contact tracing app - main.py
@@ -17,68 +17,71 @@ title.create_image(0,0, image= background)
 
 def win1():
     def win2():
+        lbl_main.destroy()
         start_btn.destroy() 
-        btn1 = tk.Button (title, text = "Submit", width=8, height=2, activebackground= "cyan" ) 
+        btn1 = tk.Button (title, text = "Submit", width=8, height=2, activebackground= "cyan") 
         btn1.place (x=210, y=450)
         
         # title
-        lbl_name = tk.Label (title, text = "CONTACT INFORMATION", font="arial")
-        lbl_name.place(x=125)
+        lbl_name = tk.Label (title, text = "CONTACT INFORMATION", font="arial", bg="white", foreground="black")
+        lbl_name.place(x=125, y=5)
 
         # name info
-        lbl_name = tk.Label (title, text = "Name")
+        lbl_name = tk.Label (title, text = "Name", bg="white")
         lbl_name.place(x=10, y=55)
         entry_name = tk.Entry (title, width="30")
         entry_name.place(x=70,y=55)
         
         # email info
-        lbl_email = tk.Label (title, text = "Email")
+        lbl_email = tk.Label (title, text = "Email", bg="white")
         lbl_email.place(x=10, y=85)
         entry_email = tk.Entry (title, width="30")
         entry_email.place(x=70,y=85)
 
         # birthday info
-        lbl_birt = tk.Label (title, text = "Birthday")
+        lbl_birt = tk.Label (title, text = "Birthday", bg="white")
         lbl_birt.place(x=10, y=115)
         entry_birt = tk.Entry (title, width="30")
         entry_birt.place(x=70,y=115)
         
         # age info
-        lbl_age = tk.Label (title, text = "Age")
+        lbl_age = tk.Label (title, text = "Age", bg="white")
         lbl_age.place(x=10, y=145)
         entry_age = tk.Entry (title, width="30")
         entry_age.place(x=70,y=145)
 
         # address info
-        lbl_add = tk.Label (title, text = "Address")
+        lbl_add = tk.Label (title, text = "Address", bg="white")
         lbl_add.place(x=10, y=175)
         entry_add = tk.Entry (title, width="30")
         entry_add.place(x=70,y=175)
 
         # vaccination
-        lbl_vax = tk.Label (title, text = "Have you been vaccinated?")
+        lbl_vax = tk.Label (title, text = "Have you been vaccinated?", bg="white")
         lbl_vax.place(x=10, y=205)
-        chk_vax1 = tk.Checkbutton(title, text="Yes", activebackground="green")
+        chk_vax1 = tk.Checkbutton(title, text="Yes", activebackground="green", bg="white")
         chk_vax1.place(x=170,y=205)
-        chk_vax2 = tk.Checkbutton(title, text="No", activebackground="green")
+        chk_vax2 = tk.Checkbutton(title, text="No", activebackground="green", bg="white")
         chk_vax2.place(x=215,y=205)
 
         # close contact
-        lbl_con = tk.Label (title, text = "Have you had exposure to a probable or confirmed case in the last 14 days?")
+        lbl_con = tk.Label (title, text = "Have you had exposure to a probable or confirmed case in the last 14 days?", bg="white")
         lbl_con.place(x=10, y=240)
-        chk_con = tk.Checkbutton(title, text="Yes", activebackground="green")
+        chk_con = tk.Checkbutton(title, text="Yes", activebackground="green", bg="white")
         chk_con.place(x=170,y=260)
-        chk_con = tk.Checkbutton(title, text="No", activebackground="green")
+        chk_con = tk.Checkbutton(title, text="No", activebackground="green", bg="white")
         chk_con.place(x=215,y=260)
 
         # experience symptoms
-        lbl_sym = tk.Label (title, text = "Are you experiencing any COVID-19 symptoms in the past 7 days?")
+        lbl_sym = tk.Label (title, text = "Are you experiencing any COVID-19 symptoms in the past 7 days?", bg="white")
         lbl_sym.place(x=10, y=290)
-        chk_sym = tk.Checkbutton(title, text="Yes", activebackground="green")
+        chk_sym = tk.Checkbutton(title, text="Yes", activebackground="green", bg="white")
         chk_sym.place(x=170,y=310)
-        chk_sym = tk.Checkbutton(title, text="No", activebackground="green")
+        chk_sym = tk.Checkbutton(title, text="No", activebackground="green", bg="white")
         chk_sym.place(x=215,y=310)
     
+    lbl_main= tk.Label (title, text = "COVID CONTACT TRACING APP", bg="white", foreground="blue", font="helvetica")
+    lbl_main.place(x=100, y=250)
     start_btn = tk.Button (title, text = "Start", width=15, height=4, activebackground= "cyan", command=win2 ) 
     start_btn.place (x=188, y=310)
 
